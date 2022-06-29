53f1ebbc827265c19faa36f077d1e0b46e56ea32
yellowk4 <yellowk4@hunet.co.kr>
2022-06-29 15:33:14 +0900

    1일차

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/Input";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<nav>
+				<Link to="/">Home</Link>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<Link to="/about">About</Link>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<Link to="/counter">Counter</Link>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<Link to="/input">Input</Link>
+			</nav>
+			<Routes>
+				<Route path="/" element={ <Home /> } />
+				<Route path="/about" element={ <About /> } />
+				<Route path="/counter" element={ <Counter /> } />
+				<Route path="/input" element={ <Input /> } />
+			</Routes>
     </div>
   );
 }
